d13e205858fc7ba6ff4879b362db59077ab24e49
leanhhuy0709 <78292068+leanhhuy0709@users.noreply.github.com>
2022-12-04 18:04:11 +0700

    Update Chat.js

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -2,6 +2,7 @@ import Navbar from "../navbar/Navbar";
 import React, { useContext } from "react";
 import GlobalContext from "../../context/GlobalContext";
 import { Link } from "react-router-dom";
+import './chat.css'
 
 const Chat = () => {
   const { user, getPersonById, conversationsList, getLastConversationMessage } =
@@ -10,35 +11,36 @@ const Chat = () => {
   return (
     <main>
       <Navbar />
-      <div className="container">
-        <div className="list-container">
-          {conversationsList.map((conversation) => {
-            const person = getPersonById(
-              conversation.users.filter((userId) => userId !== user.id)[0]
-            );
-            const lastMessageContent =
-              getLastConversationMessage(conversation.id).senderId === user.id
-                ? `You: ${getLastConversationMessage(conversation.id).content}`
-                : `${person.firstName}: ${
-                    getLastConversationMessage(conversation.id).content
-                  }`;
-            return (
-              <Link to={`/conversation/${conversation.id}`}>
-                <div className="conversation-container">
-                  <img src={person.avatar} alt="Person's avatar" />
-                  <h3>{`${person.firstName} ${person.lastName}`}</h3>
-                  <div className="last-message-container">
-                    <p className="content">{lastMessageContent}</p>
-                    <p className="timestamp">
-                      {getLastConversationMessage(conversation.id).timestamp}
-                    </p>
-                  </div>
-                  <i className="fa-solid fa-ellipsis-vertical"></i>
+      <div className="center">
+        {conversationsList.map((conversation) => {
+          const person = getPersonById(
+            conversation.users.filter((userId) => userId !== user.id)[0]
+          );
+          const lastMessageContent =
+            getLastConversationMessage(conversation.id).senderId === user.id
+              ? `You: ${getLastConversationMessage(conversation.id).content}`
+              : `${person.firstName}: ${
+                  getLastConversationMessage(conversation.id).content
+                }`;
+          return (
+            <Link to={`/conversation/${conversation.id}`} className = 'left'>
+              <div className="conversation-container">
+                <div className = "conversation-container-image">
+                  <img className = "rounded-image-medium" src={person.avatar} alt="Person's avatar" />
                 </div>
-              </Link>
-            );
-          })}
-        </div>
+                <div className = "conversation-container-text">
+                <h3>{`${person.firstName} ${person.lastName}`}</h3>
+                <div className="last-message-container">
+                  <p className="content">{lastMessageContent}</p>
+                  <p className="timestamp">
+                    {getLastConversationMessage(conversation.id).timestamp}
+                  </p>
+                </div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </main>
   );
