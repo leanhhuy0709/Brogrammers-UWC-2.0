ecf60d77dec2b23f504530d479abe6826aba873b
leanhhuy0709 <78292068+leanhhuy0709@users.noreply.github.com>
2022-12-08 16:06:36 +0700

    Update EmployeeProfile.js

--- client/src/components/employeeProfile/EmployeeProfile.js
+++ client/src/components/employeeProfile/EmployeeProfile.js
@@ -44,40 +44,45 @@ const EmployeeProfile = () => {
         <div className = "title"><h1>List of tasks </h1></div>
         <div className = 'container-2 border-blue'>
           <div className = 'profile-image center'>
-            <img className = 'rounded-image-large' src={employee.avatar} alt='avt'/>
+            <img className = 'rounded-image-medium' src={employee.avatar} alt='avt'/>
           </div>
           <div className = 'profile-text'>
-            <h1>{`${employee.firstName} ${employee.lastName}`}</h1>
-            <h6 className="employee-profile-username">@{employee.username}</h6>
-            <p>{employee.position ? "Collector" : "Janitor"}</p>
-            <p>{!employee.status ? "Online" : "Offline"}</p>
+            <h2>{`${employee.firstName} ${employee.lastName}`}</h2>
+            <p><b>@{employee.username}</b></p>
+            <p>{!employee.status ? 
+            (<div className='available-container'><span className='dot'></span>Available</div>) : 
+            (<div className='unavailable-container'><span className='dot-2'></span>Unavailable</div>)}</p>
+            
+            </div>
+            <div className = 'profile-text-2'>
+            <p>Position: {employee.position ? "Collector" : "Janitor"}</p>
+            <br/>
             <p>Member since {employee.memberSince}</p>
           </div>
         </div>
-        <table className = 'table'>
+        <table className = 'table-2'>
           <tbody>
             <tr>
-              <th className = 'table-item'>
+              <th className = 'table-item-2'>
                 RouteID
                 <span onClick={() => HandleOnSort("routeId", 0)}>
                   {reverse[0] ? <CaretDownFill />
                     : <CaretUpFill />}
                 </span>
               </th>
-              <th className = 'table-item'>First MCP</th>
-              <th className = 'table-item'>Last MCP</th>
-              <th className = 'table-item'>Time</th>
+              <th className = 'table-item-2'>First MCP</th>
+              <th className = 'table-item-2'>Last MCP</th>
+              <th className = 'table-item-2'>Time completed</th>
               {/* <th className = 'table-item'>Status</th> */}
               <th/>
             </tr>
             {!tasks.length ? <></> :
               tasks.map((value, index) => {
                 return (<tr key={value.id}>
-                  <td className = 'table-item center'>{value.routeId}</td>
-                  <td className = 'table-item center'>{value.firstMCP}</td>
-                  <td className = 'table-item center'>{value.lastMCP}</td>
-                  <td className = 'table-item center'>{value.timestamp}</td>
-                  {/* <td className = 'table-item center'>Finish ?_?</td> */}
+                  <td className = 'table-item-2 center'>{value.routeId}</td>
+                  <td className = 'table-item-2 center'>{value.firstMCP}</td>
+                  <td className = 'table-item-2 center'>{value.lastMCP}</td>
+                  <td className = 'table-item-2 center'>{value.timestamp}</td>
                 </tr>)
               })}
           </tbody>
